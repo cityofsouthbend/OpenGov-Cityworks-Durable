f431b9b2185b0ecf7b8a8d6b7771d50d5930d909
Jeff Haupt <jhaupt@southbendin.gov>
2026-01-27 08:44:36 -0500

    Initial commit

---
+++ src/functions/Cityworks-OpenGov-Orchestrator.js
@@ -0,0 +1,34 @@
+const { app } = require('@azure/functions');
+const df = require('durable-functions');
+
+const activityName = 'Cityworks-OpenGov-Orchestrator';
+
+df.app.orchestration('Cityworks-OpenGov-OrchestratorOrchestrator', function* (context) {
+    const outputs = [];
+    const cwToken = yield context.df.callActivity(getCWToken);
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+});
+
+df.app.activity(activityName, {
+    handler: (input) => {
+        return `Hello, ${input}`;
+    },
+});
+
+app.http('Cityworks-OpenGov-OrchestratorHttpStart', {
+    route: 'orchestrators/{orchestratorName}',
+    extraInputs: [df.input.durableClient()],
+    handler: async (request, context) => {
+        const client = df.getClient(context);
+        const body = await request.text();
+        const instanceId = await client.startNew(request.params.orchestratorName, { input: body });
+
+        context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+        return client.createCheckStatusResponse(request, instanceId);
+    },
+});
