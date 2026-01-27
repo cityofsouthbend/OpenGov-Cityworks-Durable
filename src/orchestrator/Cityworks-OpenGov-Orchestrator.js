8dcca284966011f9c5436015a66258d24ccb36f4
Jeff Haupt <jhaupt@southbendin.gov>
2026-01-27 10:16:42 -0500

    removed extra context orchestration

--- src/orchestrator/Cityworks-OpenGov-Orchestrator.js
+++ src/orchestrator/Cityworks-OpenGov-Orchestrator.js
@@ -5,14 +5,10 @@ const activityName = 'Cityworks-OpenGov-Orchestrator';
 
 df.app.orchestration('Cityworks-OpenGov-OrchestratorOrchestrator', function* (context) {
     const body = context.df.getInput() || {};
-    const outputs = [];
     
     const cwToken = yield context.df.callActivity(getCWToken);
-    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
-    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
-    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
 
-    return outputs;
+    return true;
 });
 
 
