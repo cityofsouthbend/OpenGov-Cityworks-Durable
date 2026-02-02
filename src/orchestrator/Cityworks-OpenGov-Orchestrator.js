bf490e3227640396ef14c6489f733a5a74b822b9
Jeff Haupt <jhaupt@southbendin.gov>
2026-02-02 12:08:55 -0500

    added cronitor

--- src/orchestrator/Cityworks-OpenGov-Orchestrator.js
+++ src/orchestrator/Cityworks-OpenGov-Orchestrator.js
@@ -1,43 +1,88 @@
-const { app } = require('@azure/functions');
-const df = require('durable-functions');
-
-const activityName = 'Cityworks-OpenGov-Orchestrator';
-
-df.app.orchestration('Cityworks-OpenGov-OrchestratorOrchestrator', function* (context) {
-    const returnValues = [];
     // currently looks like this in CItyworks {"Status":"{{Text10}}","CityworksWOID":"{{WorkOrderID}}","AccelaCaseID":"{{Text1}}","AccelaInspectionID":"{{Text2}}"}
     // will now look like this {"Status":"{{Text10}}","CityworksWOID":"{{WorkOrderID}}","OpenGovID":"{{Text1}}","RecordName":"{{Text2}}"}
     // current POST API opengov-cityworks-apgbc2gth3cyftda.centralus-01.azurewebsites.net/api/orchestrators/Cityworks-OpenGov-OrchestratorOrchestrator
     // previous codemowing-ver3.azurewebsites.net/cw
-    const body = context.df.getInput() || {};
-    console.log('CW-OG ORCH Orchestrator input:', body.CityworksWOID, body.OpenGovID);
-    const cwToken = yield context.df.callActivity('token');
-    console.log('CW-OG ORCH Cityworks Token retrieved');
-    const attachments = yield context.df.callActivity('images', { cityworksToken: cwToken, orderNumber: body.CityworksWOID });
-    returnValues.push({ attachmentsFound: attachments.length });
-    console.log('CW-OG ORC attachments', attachments);    
 
+const { app } = require('@azure/functions');
+const df = require('durable-functions');
+const axios = require('axios');
+
+const activityName = 'Cityworks-OpenGov-Orchestrator';
+
+df.app.orchestration('Cityworks-OpenGov-OrchestratorOrchestrator', function* (context) {
+    // cronitor helper function 
+    function sendCronitor(params) {
+        return axios.get(monitorUrl, { params });
+    }
+
+    const body = context.df.getInput() || {};
+    const monitorUrl = process.env.CRON_URL || null;
+    const series = context.df.instanceId;
+
+    // start cronitor
+    if (!context.df.isReplaying) {
+        yield sendCronitor({
+            state: 'run',
+            series,
+            message: `Started | CityworksWOID=${body.CityworksWOID} OpenGovID=${body.OpenGovID}`
+        });
+    }
+
+    // get Cityworks token
+    const cwToken = yield context.df.callActivity('token');
+    if (!context.df.isReplaying) {
+        yield sendCronitor({
+            series,
+            message: 'Cityworks token retrieved'
+        });
+    }
+
+    // get attachments from Cityworks work order
+    const attachments = yield context.df.callActivity('images', { cityworksToken: cwToken, orderNumber: body.CityworksWOID });  
+    if (!context.df.isReplaying) {
+        yield sendCronitor({
+            series,
+            message: `Attachments found: ${attachments.length}`
+        });
+    }  
+
+    // check for and process attachments from Cityworks
     if ( attachments.length > 0 ) {
         for (let attachment of attachments) {
             const attachmentId      = attachment.Id;
             const attachmentName    = "Cityworks_" + attachmentId.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) + ".jpg";
+            
+            // creating attachment link in OpenGov
             const fileUploadResult  = yield context.df.callActivity('fileUpload', attachmentName);
             const fileID            = fileUploadResult.data.id;
             const uploadUrl         = fileUploadResult.data.attributes.uploadUrl;
-            returnValues.push({ uploadUrl });
-            const uploadResult      = yield context.df.callActivity('uploadImages', { attachmentId, cwToken, uploadUrl });
-            returnValues.push({ attachmentId, ...uploadResult });
-            const addedFileResult  = yield context.df.callActivity('attachFile', { fileID, attachmentName, id: body.OpenGovID });
-            returnValues.push({ attachedFile: addedFileResult });
+            
+            // uploading attachment to Azure Blob URL provided by OpenGov
+            yield context.df.callActivity('uploadImages', { attachmentId, cwToken, uploadUrl });
+            
+            // linking attachment to OpenGov record
+            const attachedRecord = yield context.df.callActivity('attachFile', { fileID, attachmentName, id: body.OpenGovID });
+            if (!context.df.isReplaying) {
+                yield sendCronitor({
+                series,
+                message: `Attachment added to record: ${attachedRecord.data.id}`
+            });
+    }  
         }
     }
-
+    // update OpenGov record workflow step for VPA Mowing Abatement 
     const stepID = yield context.df.callActivity('workflowUpdate', { orderNumber: body.CityworksWOID, status: body.Status, id: body.OpenGovID });
-    returnValues.push({ stepID });
-    
+    if (!context.df.isReplaying) {
+        yield sendCronitor({
+            state: 'complete',
+            series,
+            message: `Completed successfully | StepID=${stepID}`
+        });
+    }
+
     // will need to update record workflow step (may be two parts - retrieve steps to get step ID and ordinal and then update step)
 
-    return returnValues;
+    return;
 });
 
 
